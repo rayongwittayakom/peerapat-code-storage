4bf28d789aeeab80aebcaa28f653793bee7d8b11
Pheerapat Rinnasak <151990693+nokachidev1@users.noreply.github.com>
2026-07-15 10:42:23 +0700

    Update proj.c

--- proj.c
+++ proj.c
@@ -13,27 +13,27 @@ int main() {
 	scanf("%d", &haveKeys);
 	if (HP < 0 && HP > 100)
 	{
-		printf("ERR: Request intercepted by intercept.dev-service.dungeon.personal.kisurus.com");
-		printf("Thought you could overflow the counter? IN NO WAY I WOULD ALLOW THAT TO HAPPEN");
+		printf("ERR: Request intercepted by intercept.dev-service.dungeon.personal.kisurus.com\n");
+		printf("Thought you could overflow the counter? IN NO WAY I WOULD ALLOW THAT TO HAPPEN\n");
 		goto retry;
 	} else if (haveKeys != 0 && haveKeys != 1) {
-		printf("api.v2.dungeon.personal.kisurus.com return 400");
-		printf("Why would you overflow boolean???");
+		printf("api.v2.dungeon.personal.kisurus.com return 400\n");
+		printf("Why would you overflow boolean???\n");
 		goto key;
 	}
 	if (haveKeys = 1) 
 	{
 		if (HP >= 50)
 		{
-			printf("POST api.v2.dungeon.personal.kisurus.com/scene/interact/unlock_chest return 204");
-			printf("Treasure unlocked!");
+			printf("POST api.v2.dungeon.personal.kisurus.com/scene/interact/unlock_chest return 204\n");
+			printf("Treasure unlocked!\n");
 		} else if (HP < 50) 
 		{
-			printf("api.v2.dungeon.personal.kisurus.com reply with 'Insufficient health'");
-			printf("I would really recommend healing your health right now :)");
+			printf("api.v2.dungeon.personal.kisurus.com reply with 'Insufficient health'\n");
+			printf("I would really recommend healing your health right now :)\n");
 		}
 	} else {
-		printf("Heh, you thought you could lockpick your way in right? Well it ain't happening under my watch!");
+		printf("Heh, you thought you could lockpick your way in right? Well it ain't happening under my watch!\n");
 	}
 	return 0;
 }
