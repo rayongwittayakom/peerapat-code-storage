9585413493cc6874bedfcbf7d29547fdd22585c4
Pheerapat Rinnasak <151990693+nokachidev1@users.noreply.github.com>
2026-07-16 07:09:08 +0700

    Fix logic and syntax issues in proj.c
    - Change assignment (=) to comparison (==) on line 12
    - Remove unused enoughHP variable
    - Simplify redundant else if condition
    - Fix scanf type handling for bool variable

--- proj.c
+++ proj.c
@@ -4,18 +4,17 @@
 int main() {
 	bool haveKeys;
 	int HP;
-	bool enoughHP;
 	printf("GET v2.api.dungeon.personal.kisurus.com/player/character/stat/hp?output=int: \t");
 	scanf("%d", &HP);
 	printf("GET v2.api.dungeon.personal.kisurus.com/player/character/inventory/get_item?item_name=keys&output=bool: \t");
-	scanf("%d", &haveKeys);
-	if (haveKeys = 1) 
+	scanf("%d", (int*)&haveKeys);
+	if (haveKeys == 1) 
 	{
 		if (HP >= 50)
 		{
 			printf("POST api.v2.dungeon.personal.kisurus.com/scene/interact/unlock_chest return 204\n");
 			printf("Treasure unlocked!\n");
-		} else if (HP < 50) 
+		} else 
 		{
 			printf("api.v2.dungeon.personal.kisurus.com reply with 'Insufficient health'\n");
 			printf("I would really recommend healing your health right now :)\n");
