62f6e312202cdde2ea089b36bb22f3adcfea11f5
Pheerapat Rinnasak <151990693+nokachidev1@users.noreply.github.com>
2026-07-16 07:14:44 +0700

    Update proj.c

--- proj.c
+++ proj.c
@@ -4,8 +4,10 @@
 int main() {
 	bool haveKeys;
 	int HP;
+	retry:
 	printf("GET v2.api.dungeon.personal.kisurus.com/player/character/stat/hp?output=int: \t");
 	scanf("%d", &HP);
+	key:
 	printf("GET v2.api.dungeon.personal.kisurus.com/player/character/inventory/get_item?item_name=keys&output=bool: \t");
 	scanf("%d", (int*)&haveKeys);
 	if (haveKeys == 1) 
@@ -14,13 +16,18 @@ int main() {
 		{
 			printf("POST api.v2.dungeon.personal.kisurus.com/scene/interact/unlock_chest return 204\n");
 			printf("Treasure unlocked!\n");
-		} else 
-		{
+		} else if (HP < 50) {
 			printf("api.v2.dungeon.personal.kisurus.com reply with 'Insufficient health'\n");
 			printf("I would really recommend healing your health right now :)\n");
+		} else {
+			printf("ERR: Invalid value for HP");
+			goto retry;
 		}
-	} else {
+	} else if (haveKeys == 0) {
 		printf("Heh, you thought you could lockpick your way in right? Well it ain't happening under my watch!\n");
+	} else { 
+		printf("ERR: Invalid value for haveKeys");
+		goto key;
 	}
 	return 0;
 }
