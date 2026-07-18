104bb35545761f8b09da23861d59328fcbdf1706
Pheerapat Rinnasak <151990693+nokachidev1@users.noreply.github.com>
2026-07-18 03:02:47 +0000

    fix some minor issues in proj.c

--- proj.c
+++ proj.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main() {
-	bool haveKeys;
-	int HP;
-	retry:
-	printf("GET v2.api.dungeon.personal.kisurus.com/player/character/stat/hp?output=int: \t");
-	scanf("%d", &HP);
-	key:
-	printf("GET v2.api.dungeon.personal.kisurus.com/player/character/inventory/get_item?item_name=keys&output=bool: \t");
-	scanf("%d", (int*)&haveKeys);
-	if (haveKeys == 1) 
-	{
-		if (HP >= 50)
-		{
-			printf("POST api.v2.dungeon.personal.kisurus.com/scene/interact/unlock_chest return 204\n");
-			printf("Treasure unlocked!\n");
-		} else if (HP < 50) {
-			printf("api.v2.dungeon.personal.kisurus.com reply with 'Insufficient health'\n");
-			printf("I would really recommend healing your health right now :)\n");
-		} else {
-			printf("ERR: Invalid value for HP");
-			goto retry;
-		}
-	} else if (haveKeys == 0) {
-		printf("Heh, you thought you could lockpick your way in right? Well it ain't happening under my watch!\n");
-	} else { 
-		printf("ERR: Invalid value for haveKeys");
-		goto key;
-	}
-	return 0;
+int main(void) {
+    bool haveKeys;
+    int HP;
+    int keyInput;
+
+retry:
+    printf("GET v2.api.dungeon.personal.kisurus.com/player/character/stat/hp?output=int: \t");
+    scanf("%d", &HP);
+
+key:
+    printf("GET v2.api.dungeon.personal.kisurus.com/player/character/inventory/get_item?item_name=keys&output=bool: \t");
+    scanf("%d", &keyInput);
+
+    if (keyInput == 1) {
+        haveKeys = true;
+    } else if (keyInput == 0) {
+        haveKeys = false;
+    } else {
+        printf("ERR: Invalid value for haveKeys\n");
+        goto key;
+    }
+
+    if (haveKeys) {
+        if (HP >= 50) {
+            printf("POST api.v2.dungeon.personal.kisurus.com/scene/interact/unlock_chest return 204\n");
+            printf("Treasure unlocked!\n");
+        } else {
+            printf("api.v2.dungeon.personal.kisurus.com reply with 'Insufficient health'\n");
+            printf("I would really recommend healing your health right now :)\n");
+        }
+    } else {
+        printf("Heh, you thought you could lockpick your way in right? Well it ain't happening under my watch!\n");
+    }
+
+    return 0;
 }
