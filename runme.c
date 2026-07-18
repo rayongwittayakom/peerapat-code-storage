4ccbc8fe5d3bec7e13da2a85d88a73e49b4a08b4
Pheerapat Rinnasak <151990693+nokachidev1@users.noreply.github.com>
2026-07-18 09:53:15 +0700

    Fix parking fee calculation and output formatting
    Corrected output formatting and added conditions for parking fees based on duration.

--- runme.c
+++ runme.c
@@ -3,7 +3,18 @@
 int main() {
 	int duration;
 	hour:
-	printf("You have parked for how many hours?");
+	printf("You have parked for how many hours?\n");
 	scanf("%d", duration);
-	if (duration
+	if (duration < 1) {
+		printf("Free parking!");
+	} else if (duration >= 1 && duration <= 3) {
+		printf("You shall pay me %d THB\n", duration * 30);
+	} else if (duration > 3 && duration <= 6) {
+		printf("You shall pay me %d THB\n", duration * 25);
+	} else if (duration > 6) {
+		("You shall pay me %d THB\n", duration * 20);
+	} else {
+		printf("Invalid hours input. Retrying.\n");
+		goto hour;
+	}
 }
