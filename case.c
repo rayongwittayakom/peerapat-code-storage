2a6eeb26843979a6115472f0028a0477ae5a1090
Pheerapat Rinnasak <151990693+nokachidev1@users.noreply.github.com>
2026-07-18 03:07:42 +0000

    finished case.c

--- case.c
+++ case.c
@@ -2,10 +2,40 @@
 
 int main() {
 	int menucode;
+	int cup;
 	retry:
 	printf("Menu code: \t");
 	scanf("%d", &menucode);
-	
+	cup:
+	printf("Number of cups: \t");
+	scanf("%d", &cup);
+	if (cup <= 0) {
+		printf("ERR: Invalid number of cups\n");
+		goto cup;
+	}
 	switch (menucode) {
 		case 1: 
-			printf("
+			printf("You have selected Espresso\n");
+			printf("Your subtotal is: %d\n", 55 * cup);
+			break;
+		case 2:
+			printf("You have selected Latte\n");
+			printf("Your subtotal is: %d\n", 65 * cup);
+			break;
+		case 3:
+			printf("You have selected Cappuccino\n");
+			printf("Your subtotal is: %d\n", 70 * cup);
+			break;
+		case 4:
+			printf("You have selected Green Tea\n");
+			printf("Your subtotal is: %d\n", 60 * cup);
+			break;
+		case 5:
+			printf("You have selected Cocoa\n");
+			printf("Your subtotal is: %d\n", 50 * cup);
+			break;
+		default:
+			printf("ERR: Invalid menu code\n");
+			goto retry;
+	}
+}
