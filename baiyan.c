86f6a2e033d9a8a66be61bba62d33690a0d84a40
prim <ryw44522@rayongwit.ac.th>
2026-08-05 10:34:55 +0700

    bIY

---
+++ baiyan.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int s, ssr;
+    for (s=1;s<5;s++) {
+        for (ssr=1;ssr<7;ssr++) {
+            printf("#");
+        };
+        printf("\n");
+    };
+    return 0;
+}
