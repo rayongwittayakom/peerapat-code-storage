8c7f1f8bb471735179b36367c9190a6ee928f134
Pheerapat Rinnasak <151990693+nokachidev1@users.noreply.github.com>
2026-08-06 20:22:47 +0700

    do some stuffie

--- kku.ps2.sh.fish.py.env.cpp.gpg.exe.md.js.c
+++ kku.ps2.sh.fish.py.env.cpp.gpg.exe.md.js.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int one, two, thr, fou, fiv;
+    int numbers[5];
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+    }
+
+    printf("\nNumbers divisible by 3:\n");
+
+    for (i = 0; i < 5; i++) {
+        if (numbers[i] % 3 == 0) {
+            printf("%d\n", numbers[i]);
+        }
+    }
+    return 0;
 }
